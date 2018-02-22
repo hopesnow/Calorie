45552c58a67b6a1678e86984a6b9ac4119219009
y-kajita <y-kajita@landho.co.jp>
2018-02-22 12:02:44 +0900

    コントローラー対応
    USB接続のコントローラー対応

---
+++ Calorie/Assets/Common/Script/InputSetting/InputManagerSetter.cs
@@ -0,0 +1,199 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/// <summary>
+/// InputManagerを自動的に設定してくれるクラス
+/// </summary>
+public class InputManagerSetter
+{
+
+
+    /// <summary>
+    /// インプットマネージャーを再設定します。
+    /// </summary>
+    [MenuItem("Util/Reset InputManager")]
+    public static void ResetInputManager()
+    {
+        Debug.Log("インプットマネージャーの設定を開始します。");
+        InputManagerGenerator inputManagerGenerator = new InputManagerGenerator();
+
+        Debug.Log("設定を全てクリアします。");
+        inputManagerGenerator.Clear();
+
+        Debug.Log("プレイヤーごとの設定を追加します。");
+        for (int i = 0; i < 4; i++)
+        {
+            AddPlayerInputSettings(inputManagerGenerator, i);
+        }
+
+        Debug.Log("グローバル設定を追加します。");
+        AddGlobalInputSettings(inputManagerGenerator);
+
+        Debug.Log("インプットマネージャーの設定が完了しました。");
+    }
+
+    /// <summary>
+    /// グローバルな入力設定を追加する（OK、キャンセルなど）
+    /// </summary>
+    /// <param name="inputManagerGenerator">Input manager generator.</param>
+    private static void AddGlobalInputSettings(InputManagerGenerator inputManagerGenerator)
+    {
+
+        // 横方向
+        {
+            var name = "Horizontal";
+            inputManagerGenerator.AddAxis(InputAxis.CreatePadAxis(name, 0, 1));
+            inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "a", "d", "left", "right"));
+        }
+
+        // 縦方向
+        {
+            var name = "Vertical";
+            inputManagerGenerator.AddAxis(InputAxis.CreatePadAxis(name, 0, 2));
+            inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "s", "w", "down", "up"));
+        }
+
+        // 決定
+        {
+            var name = "OK";
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, "z", "joystick button 0"));
+        }
+
+        // キャンセル
+        {
+            var name = "Cancel";
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, "x", "joystick button 1"));
+        }
+
+        // ポーズ
+        {
+            var name = "Pause";
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, "escape", "joystick button 7"));
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーごとの入力設定を追加する
+    /// </summary>
+    /// <param name="inputManagerGenerator">Input manager generator.</param>
+    /// <param name="playerIndex">Player index.</param>
+    private static void AddPlayerInputSettings(InputManagerGenerator inputManagerGenerator, int playerIndex)
+    {
+        if (playerIndex < 0 || playerIndex > 3) Debug.LogError("プレイヤーインデックスの値が不正です。");
+        string upKey = "", downKey = "", leftKey = "", rightKey = "", jumpKey = "", absorbKey = "", shotKey = "";
+        GetAxisKey(out upKey, out downKey, out leftKey, out rightKey, out jumpKey, out absorbKey, out shotKey, playerIndex);
+
+        int joystickNum = playerIndex + 1;
+
+        // 横方向
+        {
+            var name = string.Format("Player{0} Horizontal", playerIndex);
+            inputManagerGenerator.AddAxis(InputAxis.CreatePadAxis(name, joystickNum, 1));
+            inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, leftKey, rightKey, "", ""));
+        }
+
+        // 縦方向
+        {
+            var name = string.Format("Player{0} Vertical", playerIndex);
+            inputManagerGenerator.AddAxis(InputAxis.CreatePadAxis(name, joystickNum, 2));
+            inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, downKey, upKey, "", ""));
+        }
+
+
+        // ジャンプ
+        {
+            var axis = new InputAxis();
+            var name = string.Format("Player{0} Jump", playerIndex);
+            var button = string.Format("joystick {0} button 0", joystickNum);
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, button, jumpKey));
+        }
+        // 吸い込む
+        {
+            var axis = new InputAxis();
+            var name = string.Format("Player{0} Absorb", playerIndex);
+            var button = string.Format("joystick {0} button 2", joystickNum);
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, button, absorbKey));
+        }
+        // ショット
+        {
+            var axis = new InputAxis();
+            var name = string.Format("Player{0} Shot", playerIndex);
+            var button = string.Format("joystick {0} button 1", joystickNum);
+            inputManagerGenerator.AddAxis(InputAxis.CreateButton(name, button, shotKey));
+        }
+    }
+
+    /// <summary>
+    /// キーボードでプレイした場合、割り当たっているキーを取得する
+    /// </summary>
+    /// <param name="upKey">Up key.</param>
+    /// <param name="downKey">Down key.</param>
+    /// <param name="leftKey">Left key.</param>
+    /// <param name="rightKey">Right key.</param>
+    /// <param name="attackKey">Attack key.</param>
+    /// <param name="playerIndex">Player index.</param>
+    private static void GetAxisKey(out string upKey, out string downKey, out string leftKey, out string rightKey, out string jumpKey, out string absorbKey, out string shotKey, int playerIndex)
+    {
+        upKey = "";
+        downKey = "";
+        leftKey = "";
+        rightKey = "";
+        jumpKey = "";
+        absorbKey = "";
+        shotKey = "";
+
+        switch (playerIndex)
+        {
+            case 0:
+                upKey = "w";
+                downKey = "s";
+                leftKey = "a";
+                rightKey = "d";
+                jumpKey = "z";
+                absorbKey = "q";
+                shotKey = "e";
+                break;
+            case 1:
+                upKey = "i";
+                downKey = "k";
+                leftKey = "j";
+                rightKey = "l";
+                jumpKey = "m";
+                absorbKey = "u";
+                shotKey = "o";
+                break;
+            case 2:
+                upKey = "up";
+                downKey = "down";
+                leftKey = "left";
+                rightKey = "right";
+                jumpKey = "[0]";
+                absorbKey = "n";
+                shotKey = "m";
+                break;
+            case 3:
+                upKey = "[8]";
+                downKey = "[5]";
+                leftKey = "[4]";
+                rightKey = "[6]";
+                jumpKey = "[1]";
+                absorbKey = "[7]";
+                shotKey = "[9]";
+                break;
+            default:
+                Debug.LogError("プレイヤーインデックスの値が不正です。");
+                upKey = "";
+                downKey = "";
+                leftKey = "";
+                rightKey = "";
+                jumpKey = "";
+                absorbKey = "";
+                shotKey = "";
+                break;
+        }
+    }
+}
+
+#endif
--- Calorie/Assets/SceneData/Game/Script/TestController.cs
+++ Calorie/Assets/SceneData/Game/Script/TestController.cs
@@ -8,6 +8,8 @@ public class TestController : MonoBehaviour
   PlayerMover mover;
   [SerializeField]
   PlayerAction action;
+  [SerializeField]
+  int playerNo;
 
   Vector3 vec;
   // Update is called once per frame
@@ -15,32 +17,32 @@ public class TestController : MonoBehaviour
   {
     vec = new Vector3(0,0,0);
 
-    if(Input.GetKey(KeyCode.W))
+    if(Input.GetAxisRaw(string.Format("Player{0} Vertical", playerNo)) > 0||Input.GetKey(KeyCode.W))
     {
       vec.z++;
     }
 
-    if (Input.GetKey(KeyCode.A))
+    if (Input.GetAxisRaw(string.Format("Player{0} Horizontal", playerNo)) < 0||Input.GetKey(KeyCode.A))
     {
       vec.x--;
     }
 
-    if (Input.GetKey(KeyCode.S))
+    if (Input.GetAxisRaw(string.Format("Player{0} Vertical", playerNo)) < 0||Input.GetKey(KeyCode.S))
     {
       vec.z--;
     }
 
-    if (Input.GetKey(KeyCode.D))
+    if (Input.GetAxisRaw(string.Format("Player{0} Horizontal", playerNo)) > 0||Input.GetKey(KeyCode.D))
     {
       vec.x++;
     }
 
-    if(Input.GetKeyDown(KeyCode.Space))
+    if(Input.GetButtonDown(string.Format("Player{0} Jump", playerNo))||Input.GetKeyDown(KeyCode.Space))
     {
       mover.Jump(1);
     }
 
-    if(Input.GetKey(KeyCode.LeftShift))
+    if(Input.GetButton(string.Format("Player{0} Absorb", playerNo))||Input.GetKey(KeyCode.LeftShift))
     {
       action.StartAbsorbtion();
     }
@@ -49,7 +51,7 @@ public class TestController : MonoBehaviour
       action.StopAbsorbtion();
     }
 
-    if(Input.GetKey(KeyCode.RightShift))
+    if(Input.GetButton(string.Format("Player{0} Shot", playerNo))||Input.GetKey(KeyCode.RightShift))
     {
       action.Attack();
     }
