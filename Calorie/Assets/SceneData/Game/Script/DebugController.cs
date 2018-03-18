ba0ccee4c9c07aaf7e02eae2c308acff7d7f950c
ryo <dzm512.myu@gmail.com>
2018-03-18 20:41:21 +0900

    放水テスト用に調整

--- Calorie/Assets/SceneData/Game/Script/DebugController.cs
+++ Calorie/Assets/SceneData/Game/Script/DebugController.cs
@@ -1,52 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 //動作確認用
 public class DebugController : MonoBehaviour
 {
-  [SerializeField]
-  PlayerController controller;
-  [SerializeField]
-  SprayWaterParticle test;
-  [SerializeField]
-  Collider[] tests;
+    [SerializeField] private PlayerController controller;
+    [SerializeField] private SprayWaterParticle test;
+    [SerializeField] private Collider[] tests;
+    [SerializeField] private float moveSpeed = 2.0f;    // １ベクトルに対する移動速度
+    [SerializeField] private bool isPlayer = true;
+    [SerializeField] private int playerNo = 0;
 
-  private void Start()
-  {
-    test.SetupCollision(0);
-    test.SetupTrigger(tests);
-  }
-
-  // Update is called once per frame
-  void Update ()
-  {
-    Vector3 vec = Vector3.zero;
-
-    if(Input.GetKey(KeyCode.W))
+    private void Start()
     {
-      vec.z += 1;
+        test.SetupCollision(this.playerNo);
+        test.SetupTrigger(tests);
     }
 
-    if (Input.GetKey(KeyCode.S))
+    // Update is called once per frame
+    void Update()
     {
-      vec.z -= 1;
+        if (!this.isPlayer)
+            return;
+
+        Vector3 vec = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            vec.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            vec.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            vec.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            vec.x += 1;
+        }
+
+        // 放水処理
+        test.SetActiveEmitter(Input.GetKey(KeyCode.Space));
+
+        // 移動処理
+        controller.Move(vec.normalized * moveSpeed);
     }
-
-    if (Input.GetKey(KeyCode.A))
-    {
-      vec.x += -1;
-    }
-
-    if (Input.GetKey(KeyCode.D))
-    {
-      vec.x += 1;
-    }
-
-    test.SetActiveEmitter(Input.GetKey(KeyCode.LeftShift));
-  
-
-    controller.Move(vec.normalized);
-
-  }
 }
--- Calorie/Assets/SceneData/Game/Script/SprayWaterParticle.cs
+++ Calorie/Assets/SceneData/Game/Script/SprayWaterParticle.cs
@@ -12,95 +12,94 @@ using UnityEngine;
 //************************************************
 public class SprayWaterParticle : MonoBehaviour
 {
-  [SerializeField]
-  ParticleSystem ps;
+    [SerializeField] private ParticleSystem ps;
+    [SerializeField] private float dischargePower = 0.5f;
 
-  List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+    List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
 
-  bool isPlaying = false;
+    bool isPlaying = false;
 
-  //衝突時処理（プレイヤー処理)
-  //プレイヤーのタグの場合は吹き飛ばす力を加算する
-  //タグが直指定なので工夫必要かも
-  private void OnParticleCollision(GameObject other)
-  {
-    int eventNum = ps.GetCollisionEvents(other, collisionEvents);
-
-    if (other.tag == "Player")
+    //衝突時処理（プレイヤー処理)
+    //プレイヤーのタグの場合は吹き飛ばす力を加算する
+    //タグが直指定なので工夫必要かも
+    private void OnParticleCollision(GameObject other)
     {
-      var mover = other.GetComponent<IPlayerMover>();
+        int eventNum = ps.GetCollisionEvents(other, collisionEvents);
 
-      for(int i = 0; i < eventNum; i++)
-      {
-        Vector3 force = collisionEvents[i].velocity * 0.5f;
-        mover.AddOutsideForce(force);       
-      }
+        if (other.tag == "Player")
+        {
+            var mover = other.GetComponent<IPlayerMover>();
+
+            for (int i = 0; i < eventNum; i++)
+            {
+                Vector3 force = collisionEvents[i].velocity * dischargePower;
+                mover.AddOutsideForce(force);
+            }
+        }
     }
 
-  }
-
-  //反射とか反射しないとかがあるため
-  //破棄したい場合はこっちで別途判定とって殺す
-  private void OnParticleTrigger()
-  {
-    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
-    int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-
-    for(int i = 0; i < numEnter; i++)
+    //反射とか反射しないとかがあるため
+    //破棄したい場合はこっちで別途判定とって殺す
+    private void OnParticleTrigger()
     {
-      var particle = enter[i];
-      particle.remainingLifetime = 0f;
-      enter[i] = particle;
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        for (int i = 0; i < numEnter; i++)
+        {
+            var particle = enter[i];
+            particle.remainingLifetime = 0f;
+            enter[i] = particle;
+        }
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
     }
 
-    ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
-  }
-
-  //発射したりしなかったり
-  public void SetActiveEmitter(bool _isPlaying)
-  {
-    if(_isPlaying && !isPlaying)
+    //発射したりしなかったり
+    public void SetActiveEmitter(bool _isPlaying)
     {
-      isPlaying = true;
-      ps.Play();
-    }
-    else if(!_isPlaying)
-    {
-      ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-      isPlaying = false;
-    }
-  }
-
-  //どのレイヤーと判定とるか
-  //自分との判定排除　直書き多いので修正必須
-  public void SetupCollision(int playerNo = 0)
-  {
-    string[] layerName = new string[5];
-
-    int cnt = 0;
-    for(int i = 0; i < 4; i ++)
-    {
-      if (i == playerNo)
-        continue;
-
-      layerName[cnt] = "Player" + i.ToString();
-      cnt++;
+        if (_isPlaying && !isPlaying)
+        {
+            isPlaying = true;
+            ps.Play();
+        }
+        else if (!_isPlaying)
+        {
+            ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            isPlaying = false;
+        }
     }
 
-    layerName[3] = "Block";
-    layerName[4] = "ReflectionObject";
-
-    var with = ps.collision;
-    with.collidesWith = LayerMask.GetMask(layerName);
-  }
-
-  //何とトリガー判定するか、
-  //ステージオブジェクト+playerになると思われ 
-  public void SetupTrigger(Collider[] coliders)
-  {
-    for (int i = 0; i < coliders.Length; i++)
+    //どのレイヤーと判定とるか
+    //自分との判定排除　直書き多いので修正必須
+    public void SetupCollision(int playerNo = 0)
     {
-      ps.trigger.SetCollider(i, coliders[i]);
+        string[] layerName = new string[5];
+
+        int cnt = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == playerNo)
+                continue;
+
+            layerName[cnt] = "Player" + i.ToString();
+            cnt++;
+        }
+
+        layerName[3] = "Block";
+        layerName[4] = "ReflectionObject";
+
+        var with = ps.collision;
+        with.collidesWith = LayerMask.GetMask(layerName);
     }
-  }
+
+    //何とトリガー判定するか、
+    //ステージオブジェクト+playerになると思われ 
+    public void SetupTrigger(Collider[] coliders)
+    {
+        for (int i = 0; i < coliders.Length; i++)
+        {
+            ps.trigger.SetCollider(i, coliders[i]);
+        }
+    }
 }
