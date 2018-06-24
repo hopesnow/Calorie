c712bbaa05644b28efc3d4ee2be4033c691d60bb
ryo <dzm512.myu@gmail.com>
2018-06-24 16:48:47 +0900

    ショットをState管理するように変更

--- Calorie/Assets/SceneData/HTSTest/Script/NormalShot.cs
+++ Calorie/Assets/SceneData/HTSTest/Script/NormalShot.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 /// </summary>
 public class NormalShot : MonoBehaviour
 {
+    public enum State
+    {
+        Accel,
+        Brake,
+        Invalid,
+    }
+
     [SerializeField] private ParticleSystem particle;
     [SerializeField] private Collider collider;
     [SerializeField] private float initSize = 1.75f;
-    [SerializeField] private float initSpeed = 0.55f;
-    [SerializeField] private float decaySpeed = 0.03f;
-    [SerializeField] private float minimumSpeed = 0.15f;
-    [SerializeField] private float knockbackPower = 5f;
 
-    private const float PowerMagni = 1000f;  // power倍率
+    [SerializeField] private float initSpeed = 0.1f;
+    [SerializeField] private float maxSpeed = 0.6f;
+    [SerializeField] private float minSpeed = 0.15f;
+
+    [SerializeField] private float upAccel = 0.1f;
+    [SerializeField] private float downAccel = 0.03f;
+
+    [SerializeField] private float knockbackPower = 5f;
 
     private Vector3 vec;
     private float speed;
@@ -25,15 +35,14 @@ public class NormalShot : MonoBehaviour
 
     private float destroyRange = 50f;
 
-    private bool shotInvalid = false;
+    private State shotState = State.Accel;
+    // private bool shotInvalid = false;
     private float deathTime = float.MaxValue;
 
     /// <summary>
     /// 初期化処理
     /// </summary>
     /// <param name="vec">方向</param>
-    /// <param name="spd">速度</param>
-    /// <param name="scale">弾のサイズ</param>
     public void Init(Vector3 vec, string parentName)// , float spd = 1.0f, float scale = 1.0f)
     {
         this.vec = vec.normalized;
@@ -47,41 +56,48 @@ public class NormalShot : MonoBehaviour
     /// </summary>
     private void Update()
     {
+        switch (this.shotState)
+        {
+            case State.Accel:   // 発射直後のショットの速度が上がるタイミング
+                // 座標の更新
+                this.transform.localPosition += this.vec * this.speed;
+                this.speed += this.upAccel;
+                if (this.speed >= this.maxSpeed)
+                {
+                    // 次のStateへ
+                    this.speed = this.maxSpeed;
+                    this.shotState = State.Brake;
+                }
 
-        if (shotInvalid)
-        {
-            if (this.deathTime <= 0)
-            {
-                Destroy(this.gameObject);
-            }
+                // 範囲外チェック
+                CheckField();
 
-            // ショットが消えるまでのカウントダウン
-            this.deathTime -= Time.deltaTime;
-        }
-        else
-        {
-            // 座標の更新
-            this.transform.localPosition += this.vec * this.speed;
-        }
+                break;
 
-        // 速度減衰処理
-        if (this.speed < minimumSpeed)
-        {
-            this.speed = minimumSpeed;
-        }
-        else
-        {
-            this.speed -= decaySpeed;
-        }
+            case State.Brake:   // 速度が上がりきって下がるタイミング
+                // 座標の更新
+                this.transform.localPosition += this.vec * this.speed;
+                this.speed -= this.downAccel;
+                if (this.speed <= this.minSpeed)
+                {
+                    // これ以上遅くはならない
+                    this.speed = this.minSpeed;
+                }
 
-        // 範囲外チェック
-        if (this.transform.localPosition.x > this.destroyRange
-            || this.transform.localPosition.z > this.destroyRange
-            || this.transform.localPosition.x < -this.destroyRange
-            || this.transform.localPosition.z < -this.destroyRange)
-        {
-            Destroy(this.gameObject);
-            // StartCoroutine(DestroyShot());
+                // 範囲外チェック
+                CheckField();
+
+                break;
+
+            case State.Invalid: // 衝突後の無効タイミング
+                if (this.deathTime <= 0)
+                {
+                    Destroy(this.gameObject);
+                }
+
+                // ショットが消えるまでのカウントダウン
+                this.deathTime -= Time.deltaTime;
+                break;
         }
     }
 
@@ -101,8 +117,25 @@ public class NormalShot : MonoBehaviour
         var player = coll.gameObject.GetComponent<PlayerController>();
         if (player != null)
         {
-            player.Knockback(this.vec, this.knockbackPower * PowerMagni * this.speed);
+            var power = this.knockbackPower * Mathf.Sqrt(this.speed);
+            player.Knockback(this.vec, power);
             InvalidShot();
+        }
+    }
+
+    /// <summary>
+    /// ステージの範囲外チェック
+    /// </summary>
+    private void CheckField()
+    {
+        // 範囲外チェック
+        if (this.transform.localPosition.x > this.destroyRange
+            || this.transform.localPosition.z > this.destroyRange
+            || this.transform.localPosition.x < -this.destroyRange
+            || this.transform.localPosition.z < -this.destroyRange)
+        {
+            // 画面外に出ると消す
+            Destroy(this.gameObject);
         }
     }
 
@@ -112,7 +145,8 @@ public class NormalShot : MonoBehaviour
     /// </summary>
     private void InvalidShot()
     {
-        this.shotInvalid = true;
+        // this.shotInvalid = true;
+        this.shotState = State.Invalid;
         this.particle.Stop();
         this.collider.enabled = false;
         this.deathTime = this.particle.main.duration;
